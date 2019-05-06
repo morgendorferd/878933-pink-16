da4a5a2a86cfbe2f19110269ce02ceb537306fa8
Дарья <d-hohlova94@mail.ru>
2019-05-06 16:56:56 +0300

    Немножечко заработало

--- source/js/main.js
+++ source/js/main.js
@@ -7,26 +7,30 @@ var form = document.querySelector(".form");
 var name = document.querySelector("[name=name]");
 var surname = document.querySelector("[name=surname]");
 var email = document.querySelector("[name=email]");
-var formLink = document.querySelector(".form__button");
+var formSubmit = document.querySelector(".form__button");
 var modalSucces = document.querySelector(".modal-succes");
 var modalFailure = document.querySelector(".modal-failure");
+var modalSuccesClose = document.querySelector(".modal-succes__button");
+var modalFailureClose = document.querySelector(".modal-failure__button");
 
 if (form) {
   form.addEventListener("submit", function (evt) {
     if(!name.value || !surname.value || !email.value) {
       evt.preventDefault ();
-      modalFailure.classList.add("modal-show");
+      modalFailure.classList.add("modal__show");
       console.log ("Заполните форму");
     } else {
-      modalSucces.classList.add("modal-show");
-    }
+      console.log ("Форма отправлена");
+      modalSucces.classList.add("modal__show");
+    };
+  });
+  modalSuccesClose.addEventListener("click", function (evt) {
+    modalSucces.classList.remove("modal__show");
+  });
+  modalFailureClose.addEventListener("click", function (evt) {
+    modalFailure.classList.remove("modal__show");
   });
 };
-
-formLink.addEventListener("click", function (evt) {
-  modalSucces.classList.add("modal-close");
-  modalFailure.classList.add("modal-close");
-})
 
 navMain.classList.remove("main-nav--nojs");
 
