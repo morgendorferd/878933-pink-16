547c4fb3eb5332f4ce23b4ef1365e3f0d338fe4d
Дарья <d-hohlova94@mail.ru>
2019-05-08 01:52:35 +0300

    Добавила оптимизацию png и jpg

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@ var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
 var csso = require("gulp-csso");
+var imagemin = require("gulp-imagemin");
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
@@ -24,6 +25,16 @@ gulp.task("css", function () {
     .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
 });
+
+gulp.task("images", function() {
+  return gulp.src("source/img/**/*.{png,jpg/svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("source/img"));
+})
 
 gulp.task("server", function () {
   server.init({
